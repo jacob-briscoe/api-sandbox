287aa0ed7a7d4100acab3c11e770bb708c856356
Jacob Briscoe <jacob.briscoe@adesa.com>
2019-01-14 11:34:44 -0500

    Security changes

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const logger = require('./infrastructure/logger');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const webLogger = require('morgan');
 const router = require('./interface/routes');
 
 const app = express();
 
+app.use(helmet());
 app.use(webLogger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
 router.install(app);
 
 logger.info('Successfully started API.');
